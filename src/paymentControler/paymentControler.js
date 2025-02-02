b88ff289c98fc379f20e19775aa2615809d44e67
Mdforidulislam <mdforidulislam962@gmail.com>
2025-02-02 19:00:11 +0600

    added new codeh here

--- src/paymentControler/paymentControler.js
+++ src/paymentControler/paymentControler.js
@@ -53,7 +53,7 @@ class PaymentController {
           {
             mode: '0011',
             payerReference: '1',
-            callbackURL: "https://server.winpay.online/bkash-callback-url", 
+            callbackURL: "https://server.win-pay.xyz/bkash-callback-url", 
             amount,
             currency: 'BDT',
             intent: 'sale',
@@ -101,7 +101,7 @@ class PaymentController {
     // If payment is canceled or failed, redirect to error page
     if (status === 'cancel' || status === 'failure') {
       console.log('Payment canceled or failed');
-      return res.redirect(`https://winpay.online`);
+      return res.redirect(`https://win-pay.xyz`);
     }
 
     // If payment is successful, process the payment
@@ -131,20 +131,20 @@ class PaymentController {
           console.log(transaction,'check all the type here  successfuly transaction!!')
 
           console.log('Payment successful, redirecting to success page');
-          return res.redirect(`https://winpay.online/profile/user`);
+          return res.redirect(`https://win-pay.xyz/profile/user`);
         } else {
           console.log('Payment failed:', data.statusMessage);
-          return res.redirect(`https://winpay.online/profile/user`);
+          return res.redirect(`https://win-pay.xyz/profile/user`);
         }
       } catch (error) {
         console.error('Error processing callback:', error.message);
-        return res.redirect(`https://winpay.online/profile/user`);
+        return res.redirect(`https://win-pay.xyz/profile/user`);
       }
     }
 
     console.log('check is return call back')
     // Handle unexpected status (for logging or debugging purposes)
-    return res.redirect(`https://winpay.online`);
+    return res.redirect(`https://win-pay.xyz`);
   }
 
   // Method to handle payment refund
